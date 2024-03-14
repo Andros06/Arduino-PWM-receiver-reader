69f3a4c8da71dfe07475749ad081b6387e912e94
Andreas Roset <andreasroset@gmail.com>
2024-03-14 19:22:52 +0100

    renamed:    src/main.cpp -> src/PWM_Receiver_Reader.cpp

--- src/PWM_Receiver_Reader.cpp
+++ src/PWM_Receiver_Reader.cpp
@@ -9,6 +9,9 @@ int mappedch1;
 int mappedch2;
 int mappedch3;
 
+const float alpha = 0.2; // Smoothing factor (adjust as needed)
+float smoothedCh3 = 0; // Variable to store smoothed value of ch3
+
 void setup() {
   pinMode(3, INPUT); // Set our input pins as such
   pinMode(5, INPUT);
@@ -22,8 +25,10 @@ void loop() {
   ch2 = pulseIn(5, HIGH, 25000); // each channel
   ch3 = pulseIn(6, HIGH, 25000);
 
-  mappedch2 = constrain(map(ch2, 2015, 1075, -100, 100), -100, 100);
-  mappedch3 = constrain(map(ch3, 1469, 1575, -100, 100), -100, 100);
+  mappedch2 = constrain(map(ch2, 2010, 1075, -100, 100), -100, 100);
+  //mappedch3 = constrain(map(ch3, 1469, 1575, -100, 100), -100, 100);
+  smoothedCh3 = alpha * ch3 + (1 - alpha) * smoothedCh3;
+  mappedch3 = constrain(map(smoothedCh3, 1475, 1570, -100, 100), -100, 100);
 
   if(ch1 > 1800){
     mappedch1 = 1;
@@ -36,14 +41,13 @@ void loop() {
     mappedch2 = 0;
   }
 
-  if( mappedch3 < 1525 && mappedch3 > 1515){
+  if( mappedch3 < 8 && mappedch3 > -8){
     mappedch3 = 0;
   }
 
   //Serial.println(mappedch1);
   //Serial.println(mappedch2);
   //Serial.println(mappedch3);
-  
 
   delay(100); 
 }
