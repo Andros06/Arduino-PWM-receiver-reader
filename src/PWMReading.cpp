c575a437a61ab3fecba566e0d5a922d70ecd94dd
Andreas Roset <andreasroset@gmail.com>
2024-03-15 20:34:27 +0100

    renamed:    src/main.cpp -> src/PWMReading.cpp

--- src/PWMReading.cpp
+++ src/PWMReading.cpp
@@ -4,23 +4,32 @@
 int ch1; // Here's where we'll keep our channel values
 int ch2;
 int ch3;
+int ch4;
 
 int mappedch1;
 int mappedch2;
 int mappedch3;
 
+unsigned long StartTime = 0;
+unsigned long currentTime = 0;
+bool Pulsing = true;
+bool Switch = false;
+
 void setup() {
   pinMode(3, INPUT); // Set our input pins as such
   pinMode(5, INPUT);
   pinMode(6, INPUT);
+  pinMode(A0, INPUT);
 
   Serial.begin(9600);
 }
 
 void loop() {
+  
   ch1 = pulseIn(3, HIGH, 25000); // Read the pulse width of 
   ch2 = pulseIn(5, HIGH, 25000); // each channel
   ch3 = pulseIn(6, HIGH, 25000);
+  ch4 = analogRead(A0);
 
   mappedch2 = constrain(map(ch2, 2015, 1075, -100, 100), -100, 100);
   mappedch3 = constrain(map(ch3, 1469, 1575, -100, 100), -100, 100);
@@ -40,9 +49,36 @@ void loop() {
     mappedch3 = 0;
   }
 
+  if((ch4 > 300) && !Switch){
+   StartTime = millis();
+   Switch = true;
+  }
+
+  if((ch4 < 200)){
+    Pulsing = true;
+    Switch = false;
+  }
+
+  if(Switch){
+    currentTime = millis();
+    if((currentTime - StartTime) > 500){
+      Pulsing = false;
+  }
+  }
+
+  
   //Serial.println(mappedch1);
   //Serial.println(mappedch2);
   //Serial.println(mappedch3);
+  //Serial.println(ch4);
+  
+  if (!Pulsing){
+    Serial.println("Backup tilkoplet!!");
+  }
+
+  if (Pulsing){
+    Serial.println("Backup er ikkje tilkoplet!!");
+  }
   
 
   delay(100); 
